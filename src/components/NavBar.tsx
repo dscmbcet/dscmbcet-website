84d07d46f2cf2068dc055894215ff5de3b4d7f1f
Hipster16 <100425953+Hipster16@users.noreply.github.com>
2023-04-19 14:42:03 +0530

    update: Navbar button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,6 +11,7 @@ import Sun from "../assets/Sun.svg";
 import Marquee from "react-fast-marquee";
 import SunMoon from "../assets/SunMoon.svg";
 import { ThemeContext } from "../context/theme-context";
+import FilledButton from "../components/FilledButton";
 import { gsap } from "gsap";
 
 function NavBar() {
@@ -130,7 +131,7 @@ function NavBar() {
                     <a href="">Resources</a>
                     <a href="">Team</a>
                     <a href="">Magazine</a>
-                    <button className="navBar_button">Join Us</button>
+                    <FilledButton text="Join Us" textColor="var(--eerie-black)" bgColor="var(--light-sky)" border={false} />
                 </div>
                 <img
                     className="navBar_SunMoon"
