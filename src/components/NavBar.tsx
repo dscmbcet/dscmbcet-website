3db1dd646e40f8458b1abd6d147552f048edade5
aurora-0025 <ritingeorge.d@gmail.com>
2023-07-27 14:12:56 +0530

    fix: navbar closed by default

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -25,7 +25,7 @@ function NavBar() {
     gsap.registerPlugin(ScrollTrigger);
     const { theme, setTheme } = useContext(ThemeContext);
     const [hidden, setHidden] = useState<boolean>(false);
-    const [toggle, setToggle] = useState<boolean>(true);
+    const [toggle, setToggle] = useState<boolean>(false);
     const [width, setWindowWidth] = useState<number>(0)
     const navRef = useRef<HTMLDivElement | undefined>(undefined);
     const sunmoonRef = useRef(null);
