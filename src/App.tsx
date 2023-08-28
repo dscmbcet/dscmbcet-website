8caa668d88e9ffe5a8c0956ac1049786b4dfc5f0
aurora-0025 <ritingeorge.d@gmail.com>
2023-08-29 03:46:50 +0530

    add: change theme to preferred user theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,70 @@
-import { useState } from 'react';
-import './App.css'
-import { ThemeContext } from './context/theme-context';
-import { Route, RouterProvider, Routes, createBrowserRouter, useLocation } from 'react-router-dom'
-import NavBar from './components/NavBar'
-import Home from './pages/home/Home';
-import Team from './pages/team/Team';
-import Events from './pages/events/Events';
-import Footer from './components/Footer';
-import Creators from './pages/creators/Creators';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ThemeContext } from "./context/theme-context";
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Team from "./pages/team/Team";
+import Events from "./pages/events/Events";
+import Footer from "./components/Footer";
+import Creators from "./pages/creators/Creators";
 
 function App() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    if (prefersDark) {
+      setTheme("dark");
+    }
+  }, []);
+
   const router = createBrowserRouter([
     {
       path: "*",
-      element: <><Home/><Footer/></>,
+      element: (
+        <>
+          <Home />
+          <Footer />
+        </>
+      ),
     },
     {
       path: "/events",
-      element: <><Events/><Footer/></>
+      element: (
+        <>
+          <Events />
+          <Footer />
+        </>
+      ),
     },
     {
       path: "/team",
-      element: <><Team/><Footer/></>
+      element: (
+        <>
+          <Team />
+          <Footer />
+        </>
+      ),
     },
     {
       path: "/creators",
-      element: <><Creators/><Footer/></>
-    }
+      element: (
+        <>
+          <Creators />
+          <Footer />
+        </>
+      ),
+    },
   ]);
 
   return (
-    <ThemeContext.Provider value={{theme, setTheme}}>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
       <div className={`theme-${theme}`}>
         <RouterProvider router={router} />
         {/* <Events/><Footer/> */}
@@ -42,7 +75,7 @@ function App() {
         </Routes> */}
       </div>
     </ThemeContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
